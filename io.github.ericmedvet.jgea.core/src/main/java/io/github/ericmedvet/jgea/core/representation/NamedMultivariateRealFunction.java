6c230d7f3b9b05eb0795774a1f1ce33ec4540212
francescorusin <rusinfrank98@gmail.com>
2024-07-23 15:07:28 +0200

    Lil Ctrl-C, Ctrl-V bug fix.

--- io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/representation/NamedMultivariateRealFunction.java
+++ io.github.ericmedvet.jgea.core/src/main/java/io/github/ericmedvet/jgea/core/representation/NamedMultivariateRealFunction.java
@@ -1,23 +1,3 @@
-/*-
- * ========================LICENSE_START=================================
- * jgea-core
- * %%
- * Copyright (C) 2018 - 2024 Eric Medvet
- * %%
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *      http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
- * =========================LICENSE_END==================================
- */
-
 package io.github.ericmedvet.jgea.core.representation;
 
 import io.github.ericmedvet.jsdynsym.core.numerical.MultivariateRealFunction;
@@ -38,14 +18,14 @@ public interface NamedMultivariateRealFunction extends MultivariateRealFunction 
   List<String> yVarNames();
 
   static NamedMultivariateRealFunction from(
-      MultivariateRealFunction mrf, List<String> xVarNames, List<String> yVarNames) {
+          MultivariateRealFunction mrf, List<String> xVarNames, List<String> yVarNames) {
     return new ComposedNamedMultivariateRealFunction(mrf, xVarNames, yVarNames);
   }
 
   default NamedMultivariateRealFunction andThen(NamedMultivariateRealFunction other) {
     if (!new HashSet<>(yVarNames()).containsAll(other.xVarNames())) {
       throw new IllegalArgumentException("Vars mismatch: required as input=%s; produced as output=%s"
-          .formatted(other.xVarNames(), yVarNames()));
+              .formatted(other.xVarNames(), yVarNames()));
     }
     NamedMultivariateRealFunction thisNmrf = this;
     return new NamedMultivariateRealFunction() {
@@ -75,33 +55,33 @@ public interface NamedMultivariateRealFunction extends MultivariateRealFunction 
   default double[] compute(double... xs) {
     if (xs.length != xVarNames().size()) {
       throw new IllegalArgumentException("Wrong number of inputs: %d expected, %d found"
-          .formatted(xVarNames().size(), xs.length));
+              .formatted(xVarNames().size(), xs.length));
     }
     Map<String, Double> output = compute(IntStream.range(0, xVarNames().size())
-        .mapToObj(i -> Map.entry(xVarNames().get(i), xs[i]))
-        .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
+            .mapToObj(i -> Map.entry(xVarNames().get(i), xs[i]))
+            .collect(Collectors.toMap(Map.Entry::getKey, Map.Entry::getValue)));
     return yVarNames().stream().mapToDouble(output::get).toArray();
   }
 
   @Override
   default NamedMultivariateRealFunction andThen(DoubleUnaryOperator f) {
     return andThen(NamedMultivariateRealFunction.from(
-        MultivariateRealFunction.from(
-            new Function<>() {
-              @Override
-              public double[] apply(double[] vs) {
-                return Arrays.stream(vs).map(f).toArray();
-              }
+            MultivariateRealFunction.from(
+                    new Function<>() {
+                      @Override
+                      public double[] apply(double[] vs) {
+                        return Arrays.stream(vs).map(f).toArray();
+                      }
 
-              @Override
-              public String toString() {
-                return "all:%s".formatted(f);
-              }
-            },
-            nOfInputs(),
-            nOfOutputs()),
-        yVarNames(),
-        yVarNames()));
+                      @Override
+                      public String toString() {
+                        return "all:%s".formatted(f);
+                      }
+                    },
+                    nOfOutputs(),
+                    nOfOutputs()),
+            yVarNames(),
+            yVarNames()));
   }
 
   @Override
